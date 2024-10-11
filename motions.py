31410df30c4541df33392d813b8af13b2d680901
jsatkun <jananis@sidefx.com>
2024-10-10 21:50:06 -0400

    Update comments in motions.py Fixed typo in euler_to_quaternion() in utilities.py Uploaded data from final test run Created Jupyter Notebook to generate plots

--- motions.py
+++ motions.py
@@ -1,12 +1,16 @@
-#Simon's notes: tutorial for publisher and subscriber https://docs.ros.org/en/foxy/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Py-Publisher-And-Subscriber.html#write-the-subscriber-node
+# Useful Resources:
+# Tutorial for publisher and subscriber https://docs.ros.org/en/foxy/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Py-Publisher-And-Subscriber.html#write-the-subscriber-node
+# About QOS https://discuss.px4.io/t/qos-profile-for-ros2-in-python/33043
+# IMU https://docs.ros2.org/foxy/api/sensor_msgs/msg/Imu.html 
+# Odometry https://docs.ros2.org/foxy/api/nav_msgs/msg/Odometry.html 
+# Laser https://docs.ros2.org/latest/api/sensor_msgs/msg/LaserScan.html 
+# Moving the robot https://docs.ros2.org/galactic/api/geometry_msgs/msg/Twist.html 
 
 # Imports
-import rclpy
-
-from rclpy.node import Node
-
+import rclpy # ROS2 Python API
+from rclpy.node import Node # Contains subscription/publisher methods
 from utilities import Logger, euler_from_quaternion
-from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy, HistoryPolicy
+from rclpy.qos import QoSProfile
 
 # TODO Part 3: Import message types needed: 
     # For sending velocity commands to the robot: Twist
@@ -16,62 +20,65 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-
 from rclpy.time import Time
 
-# You may add any other imports you may need/want to use below
-# import ...
-
+# Define constants for the different motion types.
 CIRCLE=0; SPIRAL=1; ACC_LINE=2
+# Define labels for motion types.
 motion_types=['circle', 'spiral', 'line']
 
+# Define the motion executioner class inheriting from Node.
 class motion_executioner(Node):
     
     def __init__(self, motion_type=0):
         
+        # Initialize the node with the name 'motion_types'
         super().__init__("motion_types")
         
         self.type=motion_type
         
         self.radius_=0.0
         
+        # Flags for checking sensor intialization status, set to False.
         self.successful_init=False
         self.imu_initialized=False
         self.odom_initialized=False
         self.laser_initialized=False
-        
-        # TODO Part 3: Create a publisher to send velocity commands by setting the proper parameters in (...)
-        #Simon's notes: Twist means velocity, "velocity" is the name of the topic, queue size=10
-        self.vel_publisher = self.create_publisher(Twist, '/velocity', 10)
-                
-        # loggers
-        self.imu_logger=Logger('imu_content_'+str(motion_types[motion_type])+'.csv', headers=["acc_x", "acc_y", "angular_z", "stamp"])
-        self.odom_logger=Logger('odom_content_'+str(motion_types[motion_type])+'.csv', headers=["x","y","th", "stamp"])
-        self.laser_logger=Logger('laser_content_'+str(motion_types[motion_type])+'.csv', headers=["ranges", "angle_increment", "stamp"])
-        
+
         # TODO Part 3: Create the QoS profile by setting the proper parameters in (...)
-        #Simon's notes: not sure if this is correct or all necessary, since I think only the depth parameter is mandatory
-        #https://discuss.px4.io/t/qos-profile-for-ros2-in-python/33043
-        qos=QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT,
-            durability=DurabilityPolicy.TRANSIENT_LOCAL,
-            history=HistoryPolicy.KEEP_LAST,
-            depth=10)
+        # Create a QoS profile with specified reliability, durability, history, and depth.
+        qos=QoSProfile(reliability=2,
+                       durability=2,
+                       history=1,
+                       depth=10)
+            
+        # TODO Part 3: Create a publisher to send velocity commands by setting the proper parameters in (...)
+        # Create a publisher for velocity commands to the '/cmd_vel' topic using the QoS profile.
+        self.vel_publisher = self.create_publisher(Twist, '/cmd_vel', qos_profile=qos)
+                
+        # Establish loggers for each sensor to record data in CSV files.
+        self.imu_logger=Logger('imu_content_'+str(motion_types[motion_type])+'.csv', headers=["acc_x", "acc_y", "angular_z", "stamp"])
+        # Note: updated Odometry data headers to record quaternion components (x, y, z, w) instead of procesing euler angles during logging.
+        self.odom_logger=Logger('odom_content_'+str(motion_types[motion_type])+'.csv', headers=["x","y","quat_x", "quat_y","quat_z", "quat_w","stamp"])
+        # Note: updated LaserScan data headers to record start angle (in radians) of scan measurements.
+        self.laser_logger=Logger('laser_content_'+str(motion_types[motion_type])+'.csv', headers=["start_angle", "ranges", "angle_increment", "stamp"])
 
         # TODO Part 5: Create below the subscription to the topics corresponding to the respective sensors
+
         # IMU subscription
-        #Simon's notes: Sensor command name, imu/data is the name of the topic, imu_callback gets called as soon as it receives a message, queue size=10
-        self.create_subscription(Imu, '/imu/data', self.imu_callback, 10)
+        # Create a subscriber node for IMU: Sensor command name, imu is the name of the topic, imu_callback gets called as soon as it receives a message.
+        self.create_subscription(Imu, '/imu', self.imu_callback, qos_profile=qos)
         
         # ENCODER subscription
-        #Simon's notes: Sensor command name, odom is the name of the topic, odom_callback gets called as soon as it receives a message, queue size=10
-        self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        # Create a subscriber node for odom: Sensor command name, odom is the name of the topic, odom_callback gets called as soon as it receives a message.
+        self.create_subscription(Odometry, '/odom', self.odom_callback, qos_profile=qos)
         
         # LaserScan subscription 
-        #Simon's notes: Sensor command name, scan is the name of the topic, laser_callback gets called as soon as it receives a message, queue size=10
-        self.create_subscription(LaserScan, '/scan', self.laser_callback, 10)
+        # Create a subscriber node for laser: Sensor command name, scan is the name of the topic, laser_callback gets called as soon as it receives a message.
+        self.create_subscription(LaserScan, '/scan', self.laser_callback, qos_profile=qos)
         
+        # Establish timer to periodically execute velocity commands (every 0.1 seconds).
         self.create_timer(0.1, self.timer_callback)
-
 
     # TODO Part 5: Callback functions: complete the callback functions of the three sensors to log the proper data.
     # To also log the time you need to use the rclpy Time class, each ros msg will come with a header, and then
@@ -79,130 +86,127 @@ class motion_executioner(Node):
     # such: Time.from_msg(imu_msg.header.stamp).nanoseconds
     # You can save the needed fields into a list, and pass the list to the log_values function in utilities.py
 
-    # Simon's notes: https://docs.ros2.org/foxy/api/sensor_msgs/msg/Imu.html
+
+    # IMU callback function, logs IMU data to a CSV file.
     def imu_callback(self, imu_msg: Imu):
-        # Extract necessary data, not sure which ones we need but here's a few main ones
+        # Extract necessary data.
         acc_x = imu_msg.linear_acceleration.x
         acc_y = imu_msg.linear_acceleration.y
         angular_z = imu_msg.angular_velocity.z
         timestamp = Time.from_msg(imu_msg.header.stamp).nanoseconds
     
-        # Log the values
+        # Log the values.
         self.imu_logger.log_values([acc_x, acc_y, angular_z, timestamp])
         
-        # Mark as initialized, not sure if needed, but there is an imu_initialized = False up top so might be useful
+        # Mark IMU as initialized.
         self.imu_initialized = True
-        
-    # Simon's notes: https://docs.ros2.org/foxy/api/nav_msgs/msg/Odometry.html 
+
+
+    # Odom callback function, logs Odometry data to a CSV file.
     def odom_callback(self, odom_msg: Odometry):
-        # Extract necessary data, not sure which ones we need but here's a few main ones
+        # Extract necessary data.
         x = odom_msg.pose.pose.position.x
         y = odom_msg.pose.pose.position.y
-        roll, pitch, yaw = euler_from_quaternion(odom_msg.pose.pose.orientation) #not sure which one we are supposed to be implementing in utilities.py, modify as needed
+        quat = odom_msg.pose.pose.orientation
         timestamp = Time.from_msg(odom_msg.header.stamp).nanoseconds
 
-        # Log the values
-        self.odom_logger.log_values([x, y, yaw, timestamp])
+        # Log the values.
+        self.odom_logger.log_values([x, y, quat.x, quat.y, quat.z, quat.w, timestamp])
         
-        # Mark as initialized, not sure if needed, but there is an imu_initialized = False up top so might be useful
+        # Mark Odometry as initialized.
         self.odom_initialized = True
 
-    # Simon's notes: https://docs.ros2.org/latest/api/sensor_msgs/msg/LaserScan.html 
+
+    # Laser callback function, logs LaserScan data to a CSV file.
     def laser_callback(self, laser_msg: LaserScan):
-        # Extract necessary data, not sure which ones we need but here's a few main ones
-        ranges = list(laser_msg.ranges)
-        angle_increment = laser_msg.angle_increment
+        # Extract necessary data.
+        start_angle = laser_msg.angle_min # starting angle of the scan [rad]
+        ranges = list(laser_msg.ranges) # distance measurements [m]
+        angle_increment = laser_msg.angle_increment # angle between measurements [rad]
         timestamp = Time.from_msg(laser_msg.header.stamp).nanoseconds
         
-        # Log the values
-        self.laser_logger.log_values([ranges, angle_increment, timestamp])
+        # Log the values.
+        self.laser_logger.log_values([start_angle, ranges, angle_increment, timestamp])
         
-        # Mark as initialized, not sure if needed, but there is an imu_initialized = False up top so might be useful
+        # Mark LaserScan as initialized.
         self.laser_initialized = True
-                
+
+
+    # Timer callback function to publish velocity commands based on motion type.
     def timer_callback(self):
         
+        # Check if all sensors are initialized.
         if self.odom_initialized and self.laser_initialized and self.imu_initialized:
             self.successful_init=True
-            
         if not self.successful_init:
-            return
+            return  # Exit if sensors not initialized.
         
+        # Generate velocity command based on the selected motion type.
         cmd_vel_msg=Twist()
-        
         if self.type==CIRCLE:
             cmd_vel_msg=self.make_circular_twist()
-        
         elif self.type==SPIRAL:
-            cmd_vel_msg=self.make_spiral_twist()
-                        
+            cmd_vel_msg=self.make_spiral_twist()         
         elif self.type==ACC_LINE:
-            cmd_vel_msg=self.make_acc_line_twist()
-            
+            cmd_vel_msg=self.make_acc_line_twist()   
         else:
-            print("type not set successfully, 0: CIRCLE 1: SPIRAL and 2: ACCELERATED LINE")
-            raise SystemExit 
-
-        self.vel_publisher.publish(cmd_vel_msg)
+            print("Invalid motion type. Available options are: 0 - CIRCLE, 1 - SPIRAL, 2 - ACCELERATED LINE")
+            raise SystemExit
         
+        # Publish the velocity command
+        self.vel_publisher.publish(cmd_vel_msg)  
     
+
     # TODO Part 4: Motion functions: complete the functions to generate the proper messages corresponding to the desired motions of the robot
-    #Simon's notes: https://docs.ros2.org/galactic/api/geometry_msgs/msg/Twist.html 
-    def make_circular_twist(self):
-        
+    # Create the circular motion velocity command.
+    def make_circular_twist(self):    
         msg=Twist()
-        msg.linear.x = 0.5 #move forward
-        msg.angular.z = 0.5  #rotate
+        msg.linear.x = 0.5 # Move forward
+        msg.angular.z = 2.0 # Rotate
         return msg
 
+
+    # Create the spiral motion velocity command.
     def make_spiral_twist(self):
         msg=Twist()
         
-        self.radius_+=0.01
-        msg.linear.x = self.radius_ #increasing linear velocity
+        self.radius_ += 0.01
+        msg.linear.x = self.radius_ # Increase linear velocity
 
-        msg.angular.z=0.5 #constant rotational speed
+        msg.angular.z = 2.0 # Set a constant rotational speed
         return msg
     
+    # Create the accelerating straight line motion velocity command.
     def make_acc_line_twist(self):
         msg=Twist()
         
-        self.radius_+=0.01
-        msg.linear.x = self.radius_ #increasing linear velocity
+        self.radius_ += 0.01
+        msg.linear.x = self.radius_ # Increase linear velocity
         
-        msg.angular.z=0.0 #no rotation
+        msg.angular.z=0.0 # No rotation
         return msg
 
+# Package for reading input commands.
 import argparse
 
+# Main method to handle command-line arguments and start the ROS2 node.
 if __name__=="__main__":
-    
-
     argParser=argparse.ArgumentParser(description="input the motion type")
-
-
     argParser.add_argument("--motion", type=str, default="circle")
-
-
-
     rclpy.init()
-
     args = argParser.parse_args()
 
+    # Instantiate motion_executioner node based on the selected motion type.
     if args.motion.lower() == "circle":
-
         ME=motion_executioner(motion_type=CIRCLE)
     elif args.motion.lower() == "line":
         ME=motion_executioner(motion_type=ACC_LINE)
-
     elif args.motion.lower() =="spiral":
         ME=motion_executioner(motion_type=SPIRAL)
-
     else:
         print(f"we don't have {args.motion.lower()} motion type")
-
-
     
+    # Spin the node (keep it alive) until interrupted in the terminal.
     try:
         rclpy.spin(ME)
     except KeyboardInterrupt:
--- utilities.py
+++ utilities.py
@@ -24,8 +24,9 @@ class Logger:
             vals_str=""
 
             # TODO Part 5: Write the values from the list to the file
-            # Janani: loop through list to log values with comma separation.
+            # Log values with comma separation.
             vals_str += ", ".join(map(str, values_list))
+            vals_str += "\n"
             
             file.write(vals_str)
             
@@ -84,16 +85,17 @@ def euler_from_quaternion(quat):
     Convert quaternion (w in last place) to euler roll, pitch, yaw.
     quat = [x, y, z, w]
     """
-    # Suhyma's comment: roll = rot about x, pitch = rot about y, yaw = rot about z
+    # Unpack components of the given (unit) quaternion.
     x = quat.x
     y = quat.y
     z = quat.z
     w = quat.w
 
-    # Suhyma: Not too sure if we are supposed to implement these individually or do a bunch of matrix math...the matrix math option doesn't seem very efficient
-    roll = atan2(2*(w*x + y*z), 1 - 2*(x**2 + y**2))
-    pitch = asin(2*(w*y - z*x))
-    yaw = atan2(2*(w*x + x*y), 1 - 2*(y**2 + z**2))
+    # roll = rot about x, pitch = rot about y, yaw = rot about z
+    # Calculate yaw using this formula
+    roll = atan2(2.0 * (w * x + y * z), 1.0 - 2.0 * (x**2 + y**2))
+    pitch = asin(2.0 * (w * y - x * z))
+    yaw = atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y**2 + z**2))
 
     # just unpack yaw
     return yaw
