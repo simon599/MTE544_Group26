c7861e5a424ef082d81b9e1381c8869d0db5b935
simon599 <68410145+simon599@users.noreply.github.com>
2024-11-12 21:08:49 -0500

    Update kalman_filter.py

--- kalman_filter.py
+++ kalman_filter.py
@@ -25,7 +25,7 @@ class kalman_filter:
     def predict(self):
 
         self.A = self.jacobian_A()
-        self.C = self.jacobian_A()
+        self.C = self.jacobian_H()
         
         self.motion_model()
         
